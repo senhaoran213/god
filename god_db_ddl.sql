2108b2fe7a87aebdcff6ffa87c1642b312c32ed0
Dad <2914551291@qq.com>
2025-04-13 01:10:21 +0800

    add ddl file

---
+++ god_db_ddl.sql
@@ -0,0 +1,7 @@
+create table god_table
+(
+    id         int auto_increment
+        primary key,
+    god_column varchar(255) null comment '示例列',
+    name       varchar(255) null comment '名字'
+);
