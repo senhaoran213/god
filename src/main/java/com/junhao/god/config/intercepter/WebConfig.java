898c15549bf47ff80a2333f62c3acae118072d8d
Dad <2914551291@qq.com>
2025-06-18 23:31:42 +0800

    add WebConfig to config my jwt interceptor

---
+++ src/main/java/com/junhao/god/config/intercepter/WebConfig.java
@@ -0,0 +1,20 @@
+package com.junhao.god.config.intercepter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/**") // 需要登录的接口加这个前缀
+                .excludePathPatterns("/auth/**");
+    }
+}
