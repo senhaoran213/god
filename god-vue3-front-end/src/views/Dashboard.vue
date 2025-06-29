189701ba816791203f950374c1c31027ecf4326a
john <2914551291@qq.com>
2025-06-30 00:12:17 +0800

    1. add front end

---
+++ god-vue3-front-end/src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard-container">
+    <h1>仪表盘</h1>
+    <p>欢迎来到管理后台！</p>
+  </div>
+</template>
+
+<script setup>
+// 可添加统计数据等内容
+</script>
+
+<style scoped>
+.dashboard-container {
+  padding: 2rem;
+}
+</style> 
