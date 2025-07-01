efceae6e69e0021b2cdaea7d966d4b13b309ff7c
john <2914551291@qq.com>
2025-07-01 23:12:49 +0800

    1. 新增热门讨论点接口 2.新增ddl包

---
+++ src/main/java/com/junhao/god/service/implement/ProblemService.java
@@ -0,0 +1,9 @@
+package com.junhao.god.service.implement;
+
+import com.junhao.god.entity.Question;
+
+import java.util.List;
+
+public interface ProblemService {
+    List<Question> getProblem();
+}
