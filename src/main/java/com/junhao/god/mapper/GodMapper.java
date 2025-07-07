7ff560f2d45b26f71e867252997dd9c9dcc89332
LENOVO <jessicajia81@gmail.com>
2025-07-07 22:33:41 +0800

    1.修复接口bug

--- src/main/java/com/junhao/god/mapper/GodMapper.java
+++ src/main/java/com/junhao/god/mapper/GodMapper.java
@@ -11,10 +11,10 @@ public interface GodMapper {
     @Select("SELECT * FROM god_table")
     List<GodTable> findAll();
 
-    @Insert("INSERT INTO god_table(name, god_column) VALUES(#{name}, #{god_column})")
+    @Insert("INSERT INTO god_table(name, godColumn) VALUES(#{name}, #{god_column})")
     void insertUser(GodTable user);
 
-    @Update("UPDATE god_table SET name = #{name}, god_column = #{god_column} WHERE id = #{id}")
+    @Update("UPDATE god_table SET name = #{name}, godColumn = #{god_column} WHERE id = #{id}")
     void updateUser(GodTable user);
 
     @Delete("DELETE FROM god_table WHERE id = #{id}")
